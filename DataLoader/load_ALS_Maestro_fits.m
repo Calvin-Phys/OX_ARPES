d506e896e471759235e5b36fef9c68338d059b9c
Calvin-Phys <95595385+Calvin-Phys@users.noreply.github.com>
2024-11-20 23:31:04 -0800

    Update load_ALS_Maestro_fits.m
    Update support for ALS BL10 & 7

--- DataLoader/load_ALS_Maestro_fits.m
+++ DataLoader/load_ALS_Maestro_fits.m
@@ -3,7 +3,10 @@ function DATA = load_ALS_Maestro_fits(file_path)
 % into pre-defined objects
 %   Detailed explanation goes here
 % added support for nanoARPES
-    
+
+    % -------------------------------------------------------------------
+    % get raw data
+    % -------------------------------------------------------------------
     info = fitsinfo(file_path);
 
     keywords_primary = info.PrimaryData.Keywords;
@@ -11,6 +14,9 @@ function DATA = load_ALS_Maestro_fits(file_path)
     keywords_binarytable = info.BinaryTable.Keywords;
     fitsdata = fitsread(file_path,'binarytable');
 
+    % -------------------------------------------------------------------
+    % arpes data
+    % -------------------------------------------------------------------
     % Iterate through the keywords in the binary table of the FITS file
     % Extract values associated with keywords TDIM, TRVAL, and TDELT
     num_col = 0;
@@ -35,19 +41,43 @@ function DATA = load_ALS_Maestro_fits(file_path)
         end
     end
 
-    anglePerPixel = 0.04631; % Constant to convert from pixels to degrees
-    centerPixel = 514;
-    cXX = ( x0 + dx*(0:(nx-1)) - centerPixel) * anglePerPixel;
-    cYY = y0 + dy*(0:(ny-1));
+    if num_col ~= 0
+        anglePerPixel = 0.04631; % Constant to convert from pixels to degrees
+        centerPixel = 514;
+        cXX = ( x0 + dx*(0:(nx-1)) - centerPixel) * anglePerPixel;
+        cYY = y0 + dy*(0:(ny-1));
+    
+        NL = size(fitsdata{1,num_col},1);
+        if NL == 1
+            VALUE = reshape(fitsdata{1,num_col},nx,ny);
+        else
+            LL = round(fitsdata{1,2}',1);
+            VALUE = reshape(fitsdata{1,num_col},NL,nx,ny);
+        end
+    else % corelevel
+        for ii = 1:size(keywords_binarytable,1)
+            keyword = keywords_binarytable{ii,1};
+            if strncmp(keyword,'TRVAL',5)
 
-    NL = size(fitsdata{1,num_col},1);
-    if NL == 1
-        VALUE = reshape(fitsdata{1,num_col},nx,ny);
-    else
-        LL = round(fitsdata{1,2}',1);
-        VALUE = reshape(fitsdata{1,num_col},NL,nx,ny);
+                ny = sscanf(keywords_binarytable{ii-4,2},'%dD');
+                y0 = keywords_binarytable{ii,2};
+                dy = keywords_binarytable{ii+1,2};
+    
+                num_col = str2num(keyword(6:end));
+
+                cYY = y0 + dy*(0:(ny-1));
+                cXX = [-1, 0, 1];
+                VALUE = [fitsdata{1,num_col};fitsdata{1,num_col};fitsdata{1,num_col}];
+                break
+            end
+        end
     end
 
+   
+
+    % -------------------------------------------------------------------
+    % set parameters
+    % -------------------------------------------------------------------
     % check data type
     for ii = 1:size(keywords_primary,1)
         if strcmp(keywords_primary{ii,1},'NM_0_0')
@@ -59,7 +89,7 @@ function DATA = load_ALS_Maestro_fits(file_path)
     switch data_type
         case 'null' % cut
             DATA = OxA_CUT(cXX,cYY,VALUE);
-        case {'Slit Defl','Slit Defl.','Alpha'} % deflector map
+        case {'Slit Defl','Slit Defl.','Alpha'} %  map
             DATA = OxA_MAP(LL,cXX,cYY,VALUE);
         case 'mono_eV' % photon energy scan
             DATA = OxA_KZ(LL,cXX,cYY,VALUE);
@@ -74,14 +104,21 @@ function DATA = load_ALS_Maestro_fits(file_path)
             if startsWith(keywords_primary{ii,2},'HERS')
                 DATA.info.beamline = 'ALS_BL10';
                 DATA.info.workfunction = 4.51;
+
             elseif startsWith(keywords_primary{ii,2},'uARPES.mfast')
                 DATA.info.beamline = 'ALS_BL07_uARPES';
+                DATA.info.workfunction = 4.5;
+            elseif startsWith(keywords_primary{ii,2},'nARPES')
+                DATA.info.beamline = 'ALS_BL07_nARPES';
+                DATA.info.workfunction = 4.3;
+
             end
         elseif strcmp(keyword,'BL_E')
             beamline_energy = keywords_primary{ii,2};
             monochromator_energy = keywords_primary{ii+1,2};
-            undulator_energy = keywords_primary{ii+2,2};
-            photon_energy = round(mean([beamline_energy,monochromator_energy,undulator_energy]),2);
+            % undulator_energy = keywords_primary{ii+2,2};
+            % photon_energy = round(mean([beamline_energy,monochromator_energy,undulator_energy]),2);
+            photon_energy = round(mean([beamline_energy,monochromator_energy]),2);
             DATA.info.photon_energy = photon_energy;
         elseif strcmp(keyword,'MONOEV')
             photon_energy = round(keywords_primary{ii,2},2);
@@ -95,76 +132,22 @@ function DATA = load_ALS_Maestro_fits(file_path)
 
     DATA.info.raw = keywords_primary;
 
-    switch data_type
-        case 'null' % cut
-            DATA.y = DATA.y + DATA.info.photon_energy;
-        case {'Slit Defl','Slit Defl.','Alpha'} % deflector map
-            DATA.z = DATA.z + DATA.info.photon_energy;
+    switch DATA.info.beamline
+        case 'ALS_BL10'
+            switch data_type
+                case 'null' % cut
+                    DATA.y = DATA.y + DATA.info.photon_energy;
+                case {'Slit Defl','Slit Defl.','Alpha'} % deflector map
+                    DATA.z = DATA.z + DATA.info.photon_energy;
+            end
+        case {'ALS_BL07_uARPES','ALS_BL07_nARPES'}
+            switch data_type
+                case 'null' % cut
+                    DATA.y = DATA.y + DATA.info.photon_energy - DATA.info.workfunction;
+                case {'Slit Defl','Slit Defl.','Alpha'} % deflector map
+                    DATA.z = DATA.z + DATA.info.photon_energy - DATA.info.workfunction;
+            end
     end
-
-
-
-
-%     % Initialize variables
-%     travel = []; % Initial random value
-%     tdelt = [];
-%     
-%     % Iterate through the keywords in the binary table of the FITS file
-%     % Extract values associated with keywords TDIM, TRVAL, and TDELT
-%     for ii = 1:size(keywords_binarytable,1)
-%         keyword = keywords_binarytable{ii,1};
-%         if strncmp(keyword,'TDIM',4)
-%             tmp = sscanf(keywords_binarytable{ii,2},'(%d,%d)');
-%             nx = tmp(1);
-%             ny = tmp(2);
-%             number = keyword (5);
-%             travel = ['TRVAL',number];
-%             tdelt = ['TDELT',number];
-%         elseif strncmp(keyword,travel,6)
-%             tmp = sscanf(keywords_binarytable{ii,2},'(%f,%f)');
-%             x0 = tmp(1);
-%             y0 = tmp(2);
-%         elseif strncmp(keyword,tdelt,6)
-%             tmp = sscanf(keywords_binarytable{ii,2},'(%f,%f)');
-%             dx = tmp(1);
-%             dy = tmp(2);
-%         end
-%     end
-%     
-%     % Calculate x-axis values and convert from pixels to degrees
-%     xend = x0+dx*(nx-1);
-%     anglePerPixel = 0.04631; % Constant to convert from pixels to degrees
-%     centerPixel = (x0+xend)/2;
-%     XX = (linspace(x0,xend,nx) - centerPixel).*anglePerPixel;
-%     
-%     % Calculate y-axis values
-%     yend = y0+dy*(ny-1);
-%     YY = linspace(y0,yend,ny);
-% 
-%     % Determine the dimensionality of the data (2D or 3D)
-%     if size(fitsdata{2},1) == 1
-%         data_dim = 2;
-%     else
-%         data_dim = 3;
-%     end
-% 
-%     % Process the data based on its dimensionality
-%     switch data_dim
-%         case 2
-%             % For 2D data, reshape the data into a 2D array
-%             VALUE = reshape(fitsdata{end},nx,ny);
-% 
-%             DATA = OxA_CUT(XX,YY,VALUE);
-% 
-%         case 3
-%             % For 3D data, update axis values and reshape the data into a 3D array
-%             nz = ny;
-%             ny = nx;
-%             nx = length(fitsdata{2});
-%             VALUE = reshape(fitsdata{end},nx,ny,nz);
-% 
-%             DATA = OxA_MAP(fitsdata{2}',XX,YY,VALUE);
-%     end
 
 end
 
