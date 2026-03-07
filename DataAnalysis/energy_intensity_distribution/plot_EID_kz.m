bb751a2686ad744997386c72a38f9a11be42880d
Cheng Peng <95595385+Calvin-Phys@users.noreply.github.com>
2026-03-07 23:52:41 +0000

    Update plot_EID_kz.m

--- DataAnalysis/energy_intensity_distribution/plot_EID_kz.m
+++ DataAnalysis/energy_intensity_distribution/plot_EID_kz.m
@@ -1,17 +1,17 @@
-KZ_DATA = A015_Ni3In2S2_1_tk_s_nor;
+KZ_DATA = XXX;
 
-Texp = 100;          % Kelvin
-res_guess = 0.02;   % eV initial guess
+Texp = 26.5;          % Kelvin
+res_guess = 0.03;   % eV initial guess
 
 result = ox_calibrate_fermi_kz_scan(KZ_DATA, ...
     'Temperature', Texp, ...
     'Resolution', res_guess, ...
     'QuantileIndex', [1 2 3], ...
-    'SmoothSpan', 7, ...
+    'SmoothSpan', 1, ...
     'Debug', true);
 
 EF_curve = result.EF_smooth;
 
 %%
 
-A015_Ni3In2S2_1_tk_s_nor_fixFL = ox_apply_fermi_shift_extended(KZ_DATA,EF_curve);
+XXX_fixFL = ox_apply_fermi_shift_extended(KZ_DATA,EF_curve);
