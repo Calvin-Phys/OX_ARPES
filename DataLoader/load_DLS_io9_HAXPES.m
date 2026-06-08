74e3ee55e172067c023349be559265ec49094345
Cheng Peng <95595385+Calvin-Phys@users.noreply.github.com>
2026-06-08 17:16:41 +0100

    Update load_DLS_io9_HAXPES.m

--- DataLoader/load_DLS_io9_HAXPES.m
+++ DataLoader/load_DLS_io9_HAXPES.m
@@ -1,6 +1,6 @@
 function DATA = load_DLS_io9_HAXPES(file_path)
-%LOAD_DLS_IO9 Summary of this function goes here
-%   Detailed explanation goes here
+    %LOAD_DLS_IO9 load 1D/2D data at i09 HAXPES beamline, DLS
+    %   08/06/2026 Cheng Peng, University of Oxford
      
     %   -------------------------
     info = h5info(file_path);
@@ -10,16 +10,34 @@ function DATA = load_DLS_io9_HAXPES(file_path)
 
 
     Benergy = h5read(file_path,[region_name,'/energies']);
-    pes_total = h5read(file_path,[region_name,'/image_data']);
+    image_data = h5read(file_path,[region_name,'/image_data']);
+    dims = size(image_data);
 
-    DATA = OxArpes_1D_Data(Benergy,pes_total);
-    DATA.x_name = 'Binding Energy';
-    DATA.x_unit = 'eV';
+    if dims(1)==1 || dims(2)==1
+        pes_total = image_data;
 
-
-    DATA.info.scan_command = scan_command;
-    DATA.info.photon_energy = photon_energy;
-
+        DATA = OxArpes_1D_Data(Benergy,pes_total);
+        DATA.x_name = 'Binding Energy';
+        DATA.x_unit = 'eV';
     
+        DATA.info.scan_command = scan_command;
+        DATA.info.photon_energy = photon_energy;
+
+    else
+        angles = h5read(file_path,[region_name,'/angles']);
+
+        DATA = OxA_CUT(angles,-Benergy,image_data');
+
+        DATA.x_name = 'Angle';
+        DATA.x_unit = 'deg';
+
+        DATA.y_name = '{\itE} - {\itE}_F';
+        DATA.y_unit = 'eV';
+    
+        DATA.info.scan_command = scan_command;
+        DATA.info.photon_energy = photon_energy;
+
+    end
+
 end
 
